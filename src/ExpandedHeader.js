875f278ddb08ea5768268c880d3e6c61b00f849f
nycswan <megan.swanby@gmail.com>
2019-06-20 11:42:27 -0400

    Update styling

--- src/ExpandedHeader.js
+++ src/ExpandedHeader.js
@@ -5,33 +5,47 @@ import "./ExpandedHeader.scss";
 
 const ExpandedHeader = props => {
   return (
-    <ul
-      className={
-        props.hover === true ? "Header-list open" : "Header-list close"
-      }
-    >
-      <li
-        className={
-          props.hover === true ? "Header-link slide-up" : "Header-link close"
-        }
-      >
-        <Link to="/">Home</Link>
-      </li>
-      <li
-        className={
-          props.hover === true ? "Header-link slide-up" : "Header-link close"
-        }
-      >
-        <Link to="/about">About</Link>
-      </li>
-      <li
-        className={
-          props.hover === true ? "Header-link slide-up" : "Header-link close"
-        }
-      >
-        <Link to="/portfolio">Portfolio</Link>
-      </li>
-    </ul>
+    <div className={props.hover === true ? "m-menu open" : "m-menu"}>
+      <div className="inner">
+        <ul
+          className={props.hover === true ? "Header-list open" : "Header-list"}
+        >
+          <li
+            className={
+              props.hover === true
+                ? "Header-link slide-up"
+                : "Header-link close"
+            }
+          >
+            <Link className="text" to="/">
+              Home
+            </Link>
+          </li>
+          <li
+            className={
+              props.hover === true
+                ? "Header-link slide-up"
+                : "Header-link close"
+            }
+          >
+            <Link className="text" to="/about">
+              About
+            </Link>
+          </li>
+          <li
+            className={
+              props.hover === true
+                ? "Header-link slide-up"
+                : "Header-link close"
+            }
+          >
+            <Link className="text" to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
