c77eedf160b066afbeb6491d483f8403b9be027f
nycswan <megan.swanby@gmail.com>
2019-06-28 16:56:09 -0400

    Otimize images

---
+++ src/static/libs/About.js
@@ -0,0 +1,124 @@
+import art from "../art.jpg";
+import fam from "../fam.jpg";
+import food from "../food.jpg";
+import startups from "../startups.jpg";
+import travel from "../travel.jpg";
+import reading from "../reading.jpg";
+import hustle from "../hustle.jpg";
+import data from "../data.jpg";
+import design from "../design.jpg";
+import learn from "../learn.jpg";
+import collaboration from "../collaboration.jpg";
+import git from "../git.jpg";
+
+const topics = [
+  {
+    img: data,
+    sub:
+      "With so much available data provided by open source contributions, and transparency efforts, data visualizations are a great way to help users undertand and analyze important information and data trends. I love to sneak in a good D3 chart or visualization wherever I can.",
+    title: "Data Visualizations",
+    alt: "data visualizations",
+    cols: 2,
+    rows: 2
+  },
+  {
+    img: design,
+    sub:
+      "Design and development go hand-in-hand. If technology is not made accessable through thoughtful user-focused design, it will never succeed. As a developer who specializes in the font end, I ensure each feature keeps the user in mind with clean, functional design.",
+    title: "Design",
+    alt: "Photo by Hal Gatewood on Unsplash",
+    cols: 2,
+    rows: 2
+  },
+  {
+    img: hustle,
+    sub: "hustling",
+    title: "Work Hard",
+    alt: "logo as filler",
+    cols: 1,
+    rows: 2
+  },
+  {
+    img: travel,
+    sub:
+      "My love of traveling has introduced me to new friends, incredible adventures and a wider understanding of our world, the needs of people and the possibilities that lay before us. I have built sites with international reach",
+    title: "Travel",
+    alt: "Photo by Marten Bjork on Unsplash",
+    cols: 1,
+    rows: 3
+  },
+  {
+    img: learn,
+    title: "Education",
+    sub:
+      "As a former teacher, I take advantage of any opportunity to share my knowledge and can often be found answering CS questions to technical and non-technical teammates. Dedicated to continuously learning-- building product-related education materials with my education background, assisting the team or teaching myself a new programming language.",
+    alt: "Photo by Greg Rakozy on Unsplash",
+    cols: 2,
+    rows: 3
+  },
+  {
+    img: art,
+    title: "Art",
+    sub:
+      "When my nose is not glued to my computer, you can find me painting or touring the latest exhibits at my favorite museums.",
+    alt: "Photo by Khara Woods on Unsplash",
+    cols: 2,
+    rows: 3
+  },
+  {
+    img: startups,
+    sub:
+      "I love working with startups and non profit organizations. Utilizing my skills as a programmer, thought leader, and manager, I lead my teams and clients to success with my wide breadth of expertise, agile responsive project management and dedication to excellence.",
+    title: "Start Ups",
+    alt: "Photo by Katie Smith on Unsplash",
+    cols: 2,
+    rows: 2
+  },
+  {
+    img: reading,
+    sub:
+      "Need a good book for the beach or summary of the latest javascript text? Ask me! I've probably read it. My skills as a developer are enhanced as a book worm. I can spot an errant , or typo anywhere.",
+    title: "Book Worm",
+    alt: "Photo by Raj Eiamworakul on Unsplash",
+    cols: 1,
+    rows: 2
+  },
+  {
+    img: fam,
+    title: "Me",
+    sub:
+      "This is Rogue, my puppy and family mascot. I'm a startup founder, technology nerd, and former public school teacher, non-profit manager. I'm always up for a challenge, get in touch if you'd like a great partner",
+    alt: "Rogue -- pair programming partner, puppy extrordinary",
+    cols: 2,
+    rows: 2
+  },
+  {
+    img: food,
+    title: "Dillitant farmer & chef",
+    sub:
+      "I may live in NYC but I love growing my window sill garden and cooking anything spicy! I have been working on IoT connected gardening and home applications for the last few years. Always happy to dish on the future of IoT in farming and food!",
+    alt: "Photo by Clark Tibbs on Unsplash",
+    cols: 2,
+    rows: 3
+  },
+  {
+    img: collaboration,
+    title: "Collaboration",
+    sub:
+      "No project can be successful without deliberate, clear communcation and collaboration. I pride myself on my collaboration. I can quickly and efficiently join your team using continuous-integration tools like git or test driven development.",
+    alt: "Photo by John Schnobrich on Unsplash",
+    cols: 2,
+    rows: 3
+  },
+  {
+    img: git,
+    title: "Learn More",
+    sub:
+      "If you'd like to find out more about how I work and what I've done, please visit my linkedin page or portfolio.",
+    alt: "Photo by Yancy Min on Unsplash",
+    cols: 1,
+    rows: 3
+  }
+];
+
+export default topics;
