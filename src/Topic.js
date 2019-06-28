473445de9e32d676b23ce138a94f70f725598b04
nycswan <megan.swanby@gmail.com>
2019-06-28 16:57:23 -0400

    Update page header on index. Update home styling, about page scroll, slide y

--- src/Topic.js
+++ src/Topic.js
@@ -1,35 +1,40 @@
 import React from "react";
+import Fade from "@material-ui/core/Fade";
 import "./Topic.scss";
 import TopicJson from "./static/libs/topics.json";
 import Tags from "./Tags";
 
 export default function Topic({ match }) {
   return (
-    <div className="Topic">
-      <h3>{match.params.id.toUpperCase()}</h3>
-      {/* TODO: sort by tags */}
-      <div className="topics">
-        {TopicJson.map(topic => {
-          if (topic.topic === match.params.id) {
-            return (
-              <div key={topic.title} className="body">
-                <div className="top">
-                  <div className="header">
-                    <h5 className="title">{topic.title}</h5>
-                    <h6 className="dates">{topic.date}</h6>
+    <Fade in={true} key={match.params.id}>
+      <div className="Topic">
+        <h3>{match.params.id.toUpperCase()}</h3>
+        {/* TODO: sort by tags */}
+        <div className="topics">
+          {/*// eslint-disable-line no-use-before-define*/}
+          {TopicJson.map(topic => {
+            if (topic.topic === match.params.id) {
+              // eslint-disable-line no-use-before-define
+              return (
+                <div key={topic.title} className="body">
+                  <div className="top">
+                    <div className="header">
+                      <h5 className="title">{topic.title}</h5>
+                      <h6 className="dates">{topic.date}</h6>
+                    </div>
+                    <h6 className="sub">Role: {topic.role}</h6>
                   </div>
-                  <h6 className="sub">Role: {topic.role}</h6>
+                  <div className="bottom">
+                    <p className="text">{topic.description}</p>
+                    <p className="link">{topic.link}</p>
+                    <Tags tags={topic.tags} />
+                  </div>
                 </div>
-                <div className="bottom">
-                  <p className="text">{topic.description}</p>
-                  <p className="link">{topic.link}</p>
-                  <Tags tags={topic.tags} />
-                </div>
-              </div>
-            );
-          }
-        })}
+              );
+            }
+          })}
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 }
